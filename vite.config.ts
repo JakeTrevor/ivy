ff9bba670c0f5d704d877cb791f275c6e838f5ae
Jacob Trevor <50667410+JakeTrevor@users.noreply.github.com>
2023-05-12 10:45:23 +0100

    fixed stupid import problems

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,30 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import { tsconfigPaths } from "vite-plugin-lib";
 import dts from "vite-plugin-dts";
+import tsconfigpaths from "vite-tsconfig-paths";
+import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tsconfigPaths(), dts()],
+  plugins: [react(), tsconfigpaths(), dts()],
+  resolve: {
+    alias: {
+      "~": path.resolve(__dirname, "./src/lib"),
+    },
+  },
   build: {
     lib: {
-      entry: "./src/index.ts",
+      entry: path.resolve(__dirname, "./src/index.ts"),
       name: "index",
+    },
+    rollupOptions: {
+      external: ["react", "react-dom"],
+      output: {
+        globals: {
+          react: "React",
+          "react-dom": "ReactDOM",
+        },
+      },
     },
   },
 });
